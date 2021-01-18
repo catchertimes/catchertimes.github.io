0ebdf21ec2a7b0f1eeae95cbc359012f43f2b09c
taketheshadow <77607079+taketheshadow@users.noreply.github.com>
2021-01-18 02:13:31 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,104 @@
+//Global Variables
+//This is where you will define the variables you will be using in your project.
+//#TODO: Create four variables to track each possible quiz outcome
+var questionCount = 0;
+var result1Score = 0;
+var result2Score = 0;
+var result3Score = 0;
+var result4Score = 0;
+var totalScore = 0;
+
+//#TODO: Use the DOM to create variables for the first quiz question.
+//first question
+var q1a1 = document.getElementById("q1a1");
+var q1a2 = document.getElementById("q1a2");
+var q1a3 = document.getElementById("q1a3");
+var q1a4 = document.getElementById("q1a4");
+//second question
+var q2a1 = document.getElementById("q2a1");
+var q2a2 = document.getElementById("q2a2");
+var q2a3 = document.getElementById("q2a3");
+var q2a4 = document.getElementById("q2a4");
+//third question
+var q3a1 = document.getElementById("q3a1");
+var q3a2 = document.getElementById("q3a2");
+var q3a3 = document.getElementById("q3a3");
+var q3a4 = document.getElementById("q3a4");
+//fourth question
+var q4a1 = document.getElementById("q4a1");
+var q4a2 = document.getElementById("q4a2");
+var q4a3 = document.getElementById("q4a3");
+var q4a4 = document.getElementById("q4a4");
+//fifty question
+var q5a1 = document.getElementById("q5a1");
+var q5a2 = document.getElementById("q5a2");
+var q5a3 = document.getElementById("q5a3");
+var q5a4 = document.getElementById("q5a4");
+var result = document.getElementById("result");
+
+//#TODO: Add Event Listeners to your answer choice variables.
+q1a1.addEventListener("click", result1);
+q1a2.addEventListener("click", result2);
+q1a3.addEventListener("click", result3);
+q1a4.addEventListener("click", result4);
+
+q2a1.addEventListener("click", result1);
+q2a2.addEventListener("click", result2);
+q2a3.addEventListener("click", result3);
+q2a4.addEventListener("click", result4);
+
+q3a1.addEventListener("click", result1);
+q3a2.addEventListener("click", result2);
+q3a3.addEventListener("click", result3);
+q3a4.addEventListener("click", result4);
+
+q4a1.addEventListener("click", result1);
+q4a2.addEventListener("click", result2);
+q4a3.addEventListener("click", result3);
+q4a4.addEventListener("click", result4);
+
+q5a1.addEventListener("click", result1);
+q5a2.addEventListener("click", result2);
+q5a3.addEventListener("click", result3);
+q5a4.addEventListener("click", result4);
+
+//#TODO: Define quiz functions here
+function result1() {
+  totalScore++, questionCount++;
+  //alert("One Point to Gryffindor!");
+  if (questionCount >= 5) {
+    updateResult();
+  }
+}
+function result2() {
+  totalScore+=2, questionCount++;
+  if (questionCount >= 5) {
+    updateResult();
+  }
+}
+function result3() {
+  totalScore+=3, questionCount++;
+  if (questionCount >= 5) {
+    updateResult();
+  }
+}
+function result4() {
+  totalScore+=4, questionCount++;
+  if (questionCount >= 5) {
+    updateResult();
+  }
+}
+
+
+function updateResult() {
+  if (totalScore > 16) {
+    result.innerHTML = "You are Holden";
+  } else if (totalScore > 12) {
+    result.innerHTML = "You are Phoebe";
+  } else if (totalScore > 8) {
+    result.innerHTML = "You are Stradlater";
+  } else {
+    result.innerHTML = "You are Mr. Antolloni";
+  }
+
+}
